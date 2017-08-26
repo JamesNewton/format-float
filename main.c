8116961f005adec0115f8518b234f792ae8feb8e
JamesNewton <jamesnewton@massmind.org>
2017-08-26 12:58:59 -0700

    Fix issues #1 and #2, absq macro
    Set a padding character as a flag to avoid padding zero digits left of decimal #1
    Adding rounding correction to fix #2
    Added absq absolute value macro so stdlib isn't needed.

--- main.c
+++ main.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
 
 /*
-Because sprintf, is very costly, and all we need
+Because lcd and serial don't support printf, and its very costly, and all we need
 is simple formating with a certain number of digits and precision, this ftoa is enough.
-If digits is negative, it will pad left. 
-If precision is negative, it should stop when zero
+If digits is negative, it will pad left.
 */
 #define  BUF_LEN 20
 char buf[BUF_LEN]; //need a buffer to hold formatted strings to send to LCD
- 
-int ftoa(char * str, float f, char digits, char precision) {
+#define absq(amt) ((amt)<0?0-(amt):(amt))
+
+int ftoa(	char * str, 	//buffer to hold result.
+	float f,    	//input value
+	char digits,	//total number of digits
+	char precision	//digits right of decimal
+	) {
+long pow10[10] = {1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
 char i=0,k,l=0;
 long a,c;
-long pow10[10] = {1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000};
 unsigned char b;
 char decimal='.';
- 
-  if(digits>=10) {return 0;};
+char pad=' '; //could be a parameter to support custom padding character.
+
+  if((sizeof(pow10)/sizeof(pow10[0]))<=absq(digits)) {//larger pow10 table needed
+  	str[i++]='O';
+  	str[i]='\0';
+  	return i;
+  	}; 
   // check for negative float
-  if(f<0.0) {
-    str[i++]='-';
-    f*=-1;
-    (0<digits?digits--:digits++);
+  if(f<0.0) { //is it negative?
+    str[i++]='-'; //indicate
+    f*=-1; //make it positive
+    (0<digits?digits--:digits++); //optional, steal digit for sign, keep length
     }
   a=(int)f;	// extracting whole number
   f-=a;	// extracting decimal part
   k = digits;
   // number of digits in whole number
-  while(k>=0)	{
+  while(k>0) { //note this doesn't happen if digits was negative: Padding.
     c = pow10[k];
     c = a/c;
     if(c>0) { break; }
@@ -40,30 +49,35 @@ char decimal='.';
 extracting most significant digit i.e. right most digit , and concatenating to string
 obtained as quotient by dividing number by 10^k where k = (number of digit -1)
 */
-  for(l=abs(k);l>=0;l--){
-    c = pow10[l];
-    b = a/c;
-    str[i++]=(l&&!b?' ':b+48); //digit or pad
-    a%=c;
+  for(l=absq(k);l>=0;l--){
+    c = pow10[l];	//get the next power
+    b = a/c;	//get the next digit
+    if (b>0) pad='0';	//stop padding after first digit.
+    str[i++]=(l&&!b?pad:b+48); //digit or pad, l&& adds leading zero for fractions
+    a%=c;   	//modulo by power
     }
   if (precision) {str[i++] = decimal;};
 /* extracting decimal digits till precision */
   if (0>precision) {k=0; precision=abs(precision);}
   for(l=0;l<precision;l++) {
     f*=10.0;
-    b = (int)f; //math floor
+    b = (int)(f+0.5); //math floor so round half way
     str[i++]=b+48; //48 is ASCII 0
     f-=(float)b;
     if (!k && 0==f) { break; } //nothing left, save chars.
     //won't work if there are any floating point errors.
     }
-  str[i]='\0';
+  str[i]='\0'; //null terminate the buffer
   return i;
   }
- 
- 
+
+
 int main(void) {
-	ftoa(buf, -4444.444, -8, 3);
+	ftoa(buf, .12345, 2, 4);
+	puts(buf);
+	ftoa(buf, -4404.444, -8, 3);
+	puts(buf);
+	ftoa(buf, 4000123.1014, 6, 3);
 	puts(buf);
 	return 0;
 }
